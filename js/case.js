74e03c315d9743ba2898a56c94b11c5000c4ea16
Tutul Kabir <tutulkabir@gmail.com>
2023-08-24 22:49:35 +0530

    shopping cart handle decrease and update price

--- js/case.js
+++ js/case.js
@@ -1,8 +1,37 @@
-document.getElementById("btn-case-plus").addEventListener("click", () => {
+/* 
+1. add event listener to the case plus button
+2. get the value inside the case number field(input field)
+3. convert the number to an integer
+4. calculate the new case number
+5. set the value to the case number field
+*/
+
+function updateCaseNumber(isIncrease) {
   const caseNumberField = document.getElementById("case-number-field");
   const caseNumberString = caseNumberField.value;
-  const previousCaseNumber = parseFloat(caseNumberString);
+  const previousCaseNumber = parseInt(caseNumberString);
 
-  const newCaseNumber = previousCaseNumber + 1;
+  let newCaseNumber;
+
+  if (isIncrease === true) {
+    newCaseNumber = previousCaseNumber + 1;
+  } else {
+    newCaseNumber = previousCaseNumber - 1;
+  }
+
   caseNumberField.value = newCaseNumber;
+
+  return newCaseNumber;
+}
+
+document.getElementById("btn-case-plus").addEventListener("click", () => {
+  const newCaseNumber = updateCaseNumber(true);
+
+  const caseTotalPrice = newCaseNumber * 59;
+  const caseTotalElement = document.getElementById("case-total");
+  caseTotalElement.innerText = caseTotalPrice;
+});
+
+document.getElementById("btn-case-minus").addEventListener("click", () => {
+  updateCaseNumber(false);
 });
